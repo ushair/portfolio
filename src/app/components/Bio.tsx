d520fdb159ce99d8bb3aeb228909cdee09e00d61
ushair <ushairansari@gmail.com>
2023-07-13 00:23:35 +0530

    Added splinetool package, added bio section

--- src/app/components/Bio.tsx
+++ src/app/components/Bio.tsx
@@ -1,5 +1,19 @@
+"use client";
+import Room3D from "./Room3D";
+
 const Bio = () => {
-  return <div>Bio</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <Room3D />
+      <div className="w-4/5 text-4xl leading-[3.75rem]">
+        As a full stack developer, I am proficient in a wide range of
+        programming languages, including but not limited to HTML, CSS,
+        JavaScript, Python, and Ruby. I have hands-on experience with various
+        frameworks and libraries such as React, Angular, Node.js, Django, and
+        Ruby on Rails.
+      </div>
+    </div>
+  );
 };
 
 export default Bio;
