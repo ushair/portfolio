69f3ecfb48e0872680f390d487c52fe32cbbb5e9
ushair <ushairansari@gmail.com>
2023-07-15 00:29:49 +0530

    Created Project container.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Bio from "./components/Bio";
 import Experience from "./components/Experience";
 import Header from "./components/Header";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Bio />
       <Experience />
+      <Projects />
     </main>
   );
 }
